08f807c4c99f4008873918d91cca70c29bdd3ae9
Cory Finlinson <coryfin13@gmail.com>
2026-04-04 10:44:36 -0600

    Update theme colors

--- mobile/composeApp/src/commonMain/kotlin/com/coreo/listbox/App.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/coreo/listbox/App.kt
@@ -1,14 +1,14 @@
 package com.coreo.listbox
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.coreo.listbox.navigation.ListBoxNavHost
+import com.coreo.listbox.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    AppTheme {
         ListBoxNavHost()
     }
 }
---
+++ mobile/composeApp/src/commonMain/kotlin/com/coreo/listbox/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.coreo.listbox.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
